8dae0343a81bbc0173d3bf2cc74b250a1ed90a35
gy0kuroo <ricardoandres16d@gmail.com>
2024-12-14 15:18:42 -0300

    cambios 2

--- boti/settings.py
+++ boti/settings.py
@@ -15,6 +15,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+import pymysql
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
@@ -94,7 +95,7 @@ WSGI_APPLICATION = 'boti.wsgi.app'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
+pymysql.install_as_MySQLdb()
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
